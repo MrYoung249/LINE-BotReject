5371428a3ff10ca32a6fab7a10f394bc7f3e02f4
Juan Young <38613927+MrYoung249@users.noreply.github.com>
2019-07-13 14:10:28 +0800

    Create reject.py

---
+++ reject.py
@@ -0,0 +1,12 @@
+from linepy import *
+from time import sleep
+young = LINE()
+int1 = len(young.getGroupIdsInvited())
+if int1 == 0:
+    print("No groups inviting")
+else:
+    for groups in young.getGroupIdsInvited():
+        print("Reject " + young.getGroup(groups).name)
+        sleep(0.7)
+        maxbots.rejectGroupInvitation(groups)
+    print("\nYou reject" + str(int1) + "groups invitation")
